311f8fad2e4a245ec0605966d6ba328614c1a4d1
olgakahraman <ol.khrmn@gmail.com>
2023-04-14 21:18:33 -0400

    display sum in app

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let firstCard = 8;
-let secondCard = 6;
+let firstCard = 10;
+let secondCard = 4;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlife = true;
 let message = " ";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+
+
+function startGame(){
+
+    sumEl.textContent = "Sum: " + sum;
 
 if (sum < 21) {
     message = "Do you want to draw a new card?"
 } else if (sum ===21 ) {
-    message = "Wohoo! You've got a BlackJack!"
+    message = "You've got BlackJack!"
+    hasBlackJack = true;
 } else {
-    message = "You are out of the game"
+    message = "You are out of the game!"
+    isAlife = false;
+}
+messageEl.textContent = message;
+
 }

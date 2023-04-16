572b70d24a0d4fa72a4760565c203704af542842
olgakahraman <ol.khrmn@gmail.com>
2023-04-16 16:21:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let firstCard = 10;
 let secondCard = 4;
-let cards = [firstCard, secondCard] //array - ordered list of items
+let cards = [firstCard, secondCard]; //array - ordered list of items
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlife = true;
@@ -10,32 +10,30 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
-function startGame(){
-    renderGame();
+function startGame() {
+  renderGame();
 }
 
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  sumEl.textContent = "Sum: " + sum;
 
-function renderGame(){
-
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
-    sumEl.textContent = "Sum: " + sum;
-
-if (sum < 21) {
-    message = "Do you want to draw a new card?"
-} else if (sum ===21 ) {
-    message = "You've got Blackjack!"
+  if (sum < 21) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
     hasBlackJack = true;
-} else {
-    message = "You are out of the game!"
+  } else {
+    message = "You are out of the game!";
     isAlife = false;
+  }
+  messageEl.textContent = message;
 }
-messageEl.textContent = message;
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  let card = 7;
 
-}
-function newCard(){
-    console.log("Drawing a new card from the deck!")
-    let card = 7;
-   
-    sum += card;
-    startGame();
+  sum += card;
+  cards.push(card);
+  startGame();
 }

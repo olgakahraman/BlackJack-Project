547dcfcfde8eea0d901a06f86ccfa7ccc4dffc21
olgakahraman <ol.khrmn@gmail.com>
2023-04-22 15:30:01 -0400

    added getRandom function

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]; //array - ordered list of items
-let sum = firstCard + secondCard;
+let cards = []; //array - ordered list of items
+let sum = 0;
 let hasBlackJack = false;
-let isAlife = true;
+let isAlife = false;
 let message = " ";
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
+console.log(cards);
+
 function getRandomCard() {
   // if 1         -> return 11
   // if 11-13     -> return 10
 
   let randomNumber = Math.floor(Math.random() * 13) + 1;
-
   if (randomNumber > 10) {
     return 10;
   } else if (randomNumber === 1) {
@@ -26,17 +25,22 @@ function getRandomCard() {
 }
 
 function startGame() {
+  isAlife = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+
   renderGame();
 }
 
 function renderGame() {
   cardsEl.textContent = "Cards: ";
-
   for (let i = 0; i < cards.length; i++) {
     cardsEl.textContent += cards[i] + " ";
   }
-
-  if (sum < 21) {
+sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
     message = "You've got Blackjack!";
@@ -50,8 +54,7 @@ function renderGame() {
 function newCard() {
   console.log("Drawing a new card from the deck!");
   let card = getRandomCard();
-
   sum += card;
   cards.push(card);
-  startGame();
+  renderGame();
 }
